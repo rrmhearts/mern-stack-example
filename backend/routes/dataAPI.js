4e1a2833a66157c3e1e97b604503d554160dbef7
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-01 13:40:06 -0400

    refacted data API under routes.

---
+++ backend/routes/dataAPI.js
@@ -0,0 +1,71 @@
+var express = require("express");
+var router = express.Router();
+const mongoose = require('mongoose');
+const Data = require('./data');
+
+// this is our MongoDB database
+const dbRoute =
+    'mongodb+srv://rrmhearts:ryandb@cluster0-mpnrc.mongodb.net/test?retryWrites=true&w=majority';
+
+// connects our back end code with the database
+mongoose.connect(dbRoute, { useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+db.once('open', () => console.log('connected to the database'));
+
+// checks if connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// this is our get method
+// this method fetches all available data in our database
+router.get('/getData', (req, res) => {
+    Data.find((err, data) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true, data: data });
+    });
+});
+
+// this is our update method
+// this method overwrites existing data in our database
+router.post('/updateData', (req, res) => {
+    const { id, update } = req.body;
+    Data.findByIdAndUpdate(id, update, (err) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true });
+    });
+});
+
+// this is our delete method
+// this method removes existing data in our database
+router.delete('/deleteData', (req, res) => {
+    const { id } = req.body;
+    Data.findByIdAndRemove(id, (err) => {
+        if (err) return res.send(err);
+        return res.json({ success: true });
+    });
+});
+
+// this is our create methid
+// this method adds new data in our database
+router.post('/putData', (req, res) => {
+    let data = new Data();
+
+    const { id, message } = req.body;
+
+    if ((!id && id !== 0) || !message) {
+        return res.json({
+            success: false,
+            error: 'INVALID INPUTS',
+        });
+    }
+    data.message = message;
+    data.id = id;
+    data.save((err) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true });
+    });
+});
+
+
+module.exports = router;
